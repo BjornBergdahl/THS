c30794fa175de5aa3a2df5bc2c8c0cbd5f25888b
Björn Bergdagl <molnet@gmail.com>
2018-05-01 21:39:20 +0200

    JSF with Bootstrap startpage with navbar up and running.

---
+++ src/java/ths/THSManagedBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ths;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Björn
+ */
+@ManagedBean
+@RequestScoped
+public class THSManagedBean {
+
+    /**
+     * Creates a new instance of THSManagedBean
+     */
+    public THSManagedBean() {
+    }
+    
+}
