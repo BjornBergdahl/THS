94d9d69ddbd9f75d13cbae3bfd8a26ebf2842826
Andreas <Andreas@DESKTOP-8CFVLHA>
2018-05-11 01:09:45 +0200

    Merge origin/master. Populated the table in the "Assigned Tickets" view with some attributes of assigned tickets on init. Did some refactoring and clean up.

--- src/java/tickethandler/FXMLDocumentController.java
+++ src/java/tickethandler/FXMLDocumentController.java
@@ -12,27 +12,16 @@ import java.util.Collections;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.animation.FadeTransition;
-import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Border;
-import javafx.stage.Window;
-import javafx.stage.WindowEvent;
 import javafx.util.Duration;
-import ths.Comment;
 import ths.Ticket;
 import ths.TicketsHandler;
 
@@ -42,20 +31,16 @@ import ths.TicketsHandler;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML private Window dialog;
-    @FXML private TicketController ticketController;
-    
+    @FXML private TableView<Ticket> table;
     @FXML private AnchorPane ticketPane;
-    @FXML private ListView categoryListView;
-    
-    // FXML tags for accessing the Ticket TitledPanes
     @FXML private TitledPane ticket;
     
-    private ArrayList<Ticket> tickets = new ArrayList<>();;
+    private ArrayList<Ticket> tickets = new ArrayList<>();
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         drawPileTickets();
+        updateTable();
     }
     
     // Draws the tickets in the pile, i.e. all but the front ticket.
@@ -107,5 +92,19 @@ public class FXMLDocumentController implements Initializable {
         return th.getTickets();
     }
     
-    
+    // TODO: This method should probably be a void that updates the table
+    // When the underlying db is updated, by an observable pattern.
+    private void updateTable (){
+        
+        TicketsHandler th = new TicketsHandler();
+        th.emptyTickets();
+        th.readTickets("getAssignedTickets()");   
+        ObservableList assignedTickets = FXCollections.observableArrayList(th.getTickets());
+        
+        // table.clear();
+        table.getItems().setAll(assignedTickets);
+        table.refresh();
+        
+        System.out.println("Table loaded with data");
+    }
 }
